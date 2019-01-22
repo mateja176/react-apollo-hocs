d4fd57805b99fd20f7a51e2af9a0df7a93ee4751
Mateja Petrovic <mateja176@gmail.com>
2019-01-22 22:48:56 +0100

    Adds ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import "./App.scss";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Users from "./Users";
+
+const client = new ApolloClient({
+  uri: "https://json-placeholder-graphql.herokuapp.com/graphql"
+});
 
 const App = () => (
-  <div>
-    <div className="rocket">⁌</div>
-    <div className="gun">🔫</div>
-  </div>
+  <ApolloProvider client={client}>
+    <Users />
+  </ApolloProvider>
 );
 
 export default App;
