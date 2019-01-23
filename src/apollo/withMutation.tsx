5fe5d1a28566bd49df824c204a242d961270a1e3
Mateja Petrovic <mateja176@gmail.com>
2019-01-23 18:54:33 +0100

    Adds Variables and Data types to withMutation

--- src/apollo/withMutation.tsx
+++ src/apollo/withMutation.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Mutation, MutationResult } from "react-apollo";
 
-const withMutation = (mutation: any) => <Props extends {}>(
-  Component: React.ComponentType<Props & { mutation: any } & MutationResult>
+const withMutation = <Variables, Data>(mutation: any) => <Props extends {}>(
+  Component: React.ComponentType<
+    Props & { mutation: any } & MutationResult<Data>
+  >
 ) => (props: Props) => (
-  <Mutation mutation={mutation}>
+  <Mutation<Data, Variables> mutation={mutation}>
     {(_mutation, result) => (
       <Component mutation={_mutation} {...result} {...props} />
     )}
