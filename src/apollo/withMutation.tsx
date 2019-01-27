082d09efef8b9b79d72634e2f8d6f0a919f02c0c
Mateja Petrovic <mateja176@gmail.com>
2019-01-27 19:11:35 +0100

    Changes prop name to mutate

--- src/apollo/withMutation.tsx
+++ src/apollo/withMutation.tsx
@@ -3,13 +3,11 @@ import { Mutation, MutationFn, MutationResult } from "react-apollo";
 
 const withMutation = <Data, Variables>(mutation: any) => <Props extends {}>(
   Component: React.ComponentType<
-    Props & { mutation: MutationFn<Data, Variables> } & MutationResult<Data>
+    Props & { mutate: MutationFn<Data, Variables> } & MutationResult<Data>
   >
 ) => (props: Props) => (
   <Mutation<Data, Variables> mutation={mutation}>
-    {(_mutation, result) => (
-      <Component {...props} {...result} mutation={_mutation} />
-    )}
+    {(mutate, result) => <Component {...props} {...result} mutate={mutate} />}
   </Mutation>
 );
 
